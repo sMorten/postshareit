6226f90c4fc7aac9dd1a5a558d5829ea68f60b20
mortenso <spencer.mortenson@gmail.com>
2016-08-29 22:27:02 +0000

    Prototype app page

---
+++ app/assets/javascripts/mainapp.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+$(".mainappapp").click(function(){
+  $(".mainappapp").fadeOut(1000,function(){
+  window.location.href = "http://www.postshareit.com";
+  });
+});
+
+
+});
